022213aab67b9b9860b1af4b22f86a4c60281ce4
Kay Lee <me@kalbito.com>
2018-11-09 08:22:51 -0800

    Fix issue with Heroku

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ var path = require('path');
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
+  console.log('request');
   res.sendFile(path.resolve(__dirname + '/../client/src/index.html'))
 })
 
 app.listen(3000, function() {
-  console.log('new app listening on port 3000');
+  console.log(__dirname);
+  console.log('Anomalie listening on port 3000');
 });
